058dbc5de2b756727e54aefd1342670ac61eb131
Rodrigo Castillo <dev.rodrig@gmail.com>
2019-06-09 13:29:14 -0600

    agregando proyecto ionic

--- API/models/categories.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Categories = mongoose.Schema({
-    name: String
-}, {
-    timestamps: true
-});
-
-module.exports = mongoose.model('categories', Categories);
