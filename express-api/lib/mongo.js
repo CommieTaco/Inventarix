d8adb2d695c4b8b1636c7093f4cdf50b828441f4
CommieTaco <UFG@CC5-08.ufg.local>
2019-06-15 10:15:19 -0600

    Progreso articles

--- express-api/lib/mongo.js
+++ express-api/lib/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const {config} = require('../config')
 
-// const MONGO_URI = `mongodb://${config.dbHost}:${config.dbPort}/${config.dbName}`
-const MONGO_URI = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}`
+const MONGO_URI = `mongodb://${config.dbHost}:${config.dbPort}/${config.dbName}`
+//const MONGO_URI = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}`
 
 mongoose.Promise = global.Promise;
 
