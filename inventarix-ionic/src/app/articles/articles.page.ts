cd014d0bde2959343739256e03b7b8b07be4886b
CommieTaco <UFG@CC5-08.ufg.local>
2019-06-15 10:55:15 -0600

    Pruebas articles

--- inventarix-ionic/src/app/articles/articles.page.ts
+++ inventarix-ionic/src/app/articles/articles.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController, NavController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { error } from '@angular/compiler/src/util';
+import { ConcatSource } from 'webpack-sources';
 
 @Component({
   selector: 'app-articles',
@@ -10,25 +11,50 @@ import { error } from '@angular/compiler/src/util';
 })
 export class ArticlesPage implements OnInit {
 
-  public articles = [];
+  public artics: [];
   constructor(private menu: MenuController, public navCtrl: NavController, public http: HttpClient) { }
 
   ngOnInit() {
     
-    this.http.get('http://localhost:3000/articles/findAll')
+    this.http.get('http://localhost:3000/articles/')
     .subscribe(data =>{
 
-      this.articles = [data];
-      console.log("Data: "+this.articles);
+      
+      let pepe = JSON.stringify(data);
+      /* this.artics = pepe; */
+      console.log("POPO: "+JSON.stringify(data));
+      console.log("POPO: "+typeof data);
+      console.log("PAPA: "+Object.keys(data));
+      console.log("PAPA: "+data["name"]);
+
+      Object.keys(data).forEach( val => {
+        console.log("Val: "+val);
+      })
+      /*   this.data = data;
+      let pepe = data. */
+      /* this.articles = [data]; */
+      /* console.log("Data: "+this.articles.name); */
     }), error => {
       console.log("Hubo un error");
       if(error.status == 404)
         console.log("No se pudo encontrar");
     };
     
+    /* return this.articles; */
   }
 
   toComparison(){
     this.navCtrl.navigateRoot('/comparison');
   }
+
 }
+  export interface RootObject {
+      _id: string;
+      name: string;
+      description: string;
+      price: string;
+      url: string;
+      createdAt: Date;
+      updatedAt: Date;
+      __v: number;
+  }
